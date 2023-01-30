339c85e7296821f651d603027ff3eaf55c41fc7f
cinta <cintatafur@gmail.com>
2023-01-30 13:30:49 +0100

    🐛 Fix(img):corregido visualizar img

---
+++ src/app/pipes/imagen.pipe.ts
@@ -0,0 +1,18 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+const URL = ` https://image.tmdb.org/t/p/`;
+/* const URL = `https://image.tmdb.org/t/p/`; */
+@Pipe({
+  name: 'imagen',
+})
+export class ImagenPipe implements PipeTransform {
+  transform(img: string, size: string = 'w500'): unknown {
+
+    if(!img){
+      return;
+    }
+    const imgURL = `${URL}/${size}/${img}`;
+    console.log('URL imagen', imgURL);
+    return imgURL;
+  }
+}
