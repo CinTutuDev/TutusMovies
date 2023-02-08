26f358cb38fa751e315442827106da8afe12f3c9
cinta <cintatafur@gmail.com>
2023-02-08 13:44:16 +0100

    🚧 Working........

--- src/app/components/detalle-peli/detalle-peli.component.ts
+++ src/app/components/detalle-peli/detalle-peli.component.ts
@@ -1,5 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
-import { DetallePelis } from 'src/app/interface/interfaces';
+import { Cast, DetallePelis } from 'src/app/interface/interfaces';
 import { MoviesService } from '../../services/movies.service';
 
 @Component({
@@ -11,6 +11,7 @@ export class DetallePeliComponent implements OnInit {
   @Input() id: any;
 
   peli: DetallePelis | any = {};
+  actores: Cast[] | any = [];
   parrafo = 150;
   constructor(private moviesService: MoviesService) {}
 
@@ -24,6 +25,8 @@ export class DetallePeliComponent implements OnInit {
     /* console.log('el ID', this.id); */
     this.moviesService.getDetalleActores(this.id).subscribe((res) => {
       console.log('ID Actores', res);
+      this.actores = res.cast;
+    
     });
   }
 }
