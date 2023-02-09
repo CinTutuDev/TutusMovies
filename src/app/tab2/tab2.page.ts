50304f39ad00a7a6585fd7265b7da40b787fcb63
cinta <cintatafur@gmail.com>
2023-02-09 11:06:27 +0100

    ➕ Add(buscador):Tab2 + filtrado y muestro

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -7,6 +7,18 @@ import { Component } from '@angular/core';
 })
 export class Tab2Page {
 
+  public ideas = ['Spiderman', 'Avenger', 'El señor de los anillos', 'La vida es bella'];
+
+  public results = [...this.ideas];
+
+  buscaText = '';
+
   constructor() {}
 
+  
+  buscador(event:any){
+    console.log(event);
+    const queryValue = event.target.value.toLowerCase();
+    this.results = this.ideas.filter(d => d.toLowerCase().indexOf(queryValue) > -1);
+  }
 }
